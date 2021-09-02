46dfc63e673995797b7f455cbb5ae2a3c229f9f5
Odeyalo <theprogrammerinfuture@gmail.com>
2021-09-02 22:49:03 +0300

    now support spotify

--- src/main/java/command/commands/PlayCommand.java
+++ src/main/java/command/commands/PlayCommand.java
@@ -1,13 +1,18 @@
 package command.commands;
 
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
+import com.wrapper.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
 import command.Command;
 import connnect.ConnectToTheVoiceChannelService;
 import exceptions.SoundNotFoundException;
 import lavaplayer.PlayerManager;
 import music.SearchVideoOnYoutubeImpl;
-import net.dv8tion.jda.api.entities.VoiceChannel;
+import music.spotify.PlayMusicFromSpotify;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.managers.AudioManager;
+import org.apache.hc.core5.http.ParseException;
+import org.springframework.cglib.transform.AbstractProcessTask;
 import sendmessages.SendMessageInterface;
 
 import java.io.IOException;
@@ -21,11 +26,13 @@ public class PlayCommand implements Command {
     private ConnectToTheVoiceChannelService connect;
     private PlayerManager playerManager;
     private SearchVideoOnYoutubeImpl youtubeMusicSearcher;
+    private PlayMusicFromSpotify spotify;
 
     public PlayCommand(SendMessageInterface sendMessageInterface) {
         this.sendMessageInterface = sendMessageInterface;
         this.playerManager = new PlayerManager();
         this.youtubeMusicSearcher = new SearchVideoOnYoutubeImpl();
+        this.spotify = new PlayMusicFromSpotify();
     }
 
     @Override
@@ -46,21 +53,54 @@ public class PlayCommand implements Command {
     }
 
     private String getSongUrl(String s) throws SoundNotFoundException {
-        if(s.length() >= 5)
+        if (s.trim().length() > 5)
             return s.substring(5).trim();
         sendMessageInterface.sendMessage(event, "enter sound name");
         throw new SoundNotFoundException();
     }
-    public void play(String trackUrl)  {
+
+    public void play(String trackUrl) {
+        System.out.println("play:");
         PlayerManager.getInstance().loadAndPlay(event.getChannel(), trackUrl);
     }
 
     private void distribution(String text) throws IOException {
         try {
             new URL(text);
-            play(text);
+            if (isSpotifyURL(text)) {
+                if (isSpotifyPlaylist(text)) {
+                    String playlistId = text.substring(34, text.lastIndexOf('?'));
+                    System.out.println(playlistId);
+                    final String[] playlistItems = spotify.getPlaylistItems(playlistId);
+                    for (int i = 0; i < playlistItems.length; i++) {
+                        final String item = playlistItems[i];
+                        System.out.println(item);
+                        final String video = youtubeMusicSearcher.searchVideoByKeyword(item);
+                        play(video);
+                    }
+                } else {
+                    String songId = text.substring(25, text.lastIndexOf('?'));
+                    final String titleAndAuthorFromSpotifyApi = spotify.getTitleAndAuthorFromSpotifyApi(songId);
+                    final String songURL = youtubeMusicSearcher.searchVideoByKeyword(titleAndAuthorFromSpotifyApi);
+                    play(songURL);
+                }
+            } else {
+                play(text);
+            }
         } catch (MalformedURLException e) {
             play(youtubeMusicSearcher.searchVideoByKeyword(text));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (SpotifyWebApiException e) {
+            e.printStackTrace();
         }
+    }
+
+    public boolean isSpotifyURL(String url) {
+        return url.startsWith("https://open.spotify.com/");
+    }
+
+    public boolean isSpotifyPlaylist(String url) {
+        return url.startsWith("https://open.spotify.com/playlist/");
     }
 }
--- src/main/java/music/spotify/PlayMusicFromSpotify.java
+++ src/main/java/music/spotify/PlayMusicFromSpotify.java
@@ -1,35 +1,63 @@
 package music.spotify;
 
-import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.wrapper.spotify.SpotifyApi;
-import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
-import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
+import com.wrapper.spotify.model_objects.specification.Paging;
+import com.wrapper.spotify.model_objects.specification.Playlist;
+import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import com.wrapper.spotify.requests.data.playlists.GetPlaylistRequest;
 import com.wrapper.spotify.requests.data.tracks.GetTrackRequest;
-import exceptions.SoundNotFoundException;
-import music.Playable;
-import org.apache.commons.collections4.Get;
 import org.apache.hc.core5.http.ParseException;
 
 import java.io.IOException;
-import java.net.URI;
 
-public class PlayMusicFromSpotify  {
-    private static SpotifyApi spotifyApi;
-    private GetTrackRequest trackRequest;
-    private static AuthorizationCodeUriRequest request;
-    private static URI redirect = SpotifyHttpManager.makeUri("https://example.com/spotify-redirect");
-    public static void initialize() {
-         spotifyApi =  new SpotifyApi.Builder().
-                setClientId("2dc23db61bf344139bf140cff6fb8474")
-                .setClientSecret("198c38516b5441ddaf47593770f4fcd0")
-                 .setRedirectUri(redirect)
-                .build();
-         request = spotifyApi.authorizationCodeUri().build();
+public class PlayMusicFromSpotify {
+    private static final String clientId = "2dc23db61bf344139bf140cff6fb8474";
+    private static final String clientSecret = "198c38516b5441ddaf47593770f4fcd0";
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .build();
+    private GetTrackNameAndArtistService service;
+
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+            .build();
+
+    public PlayMusicFromSpotify() {
+        try {
+            final String accessToken = clientCredentialsRequest.execute().getAccessToken();
+            spotifyApi.setAccessToken(accessToken);
+        } catch (IOException e) {
+
+        } catch (SpotifyWebApiException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        service = new GetTrackNameAndArtistService();
+
     }
-
     public String getTitleAndAuthorFromSpotifyApi(String id) throws ParseException, SpotifyWebApiException, IOException {
         GetTrackRequest track = spotifyApi.getTrack(id).build();
-        return track.execute().getName();
+        final String[] artists = service.getArtistsName(track);
+        final String trackName = service.getTrackName(track);
+        String songFullName = "";
+        for (int i = 0; i < artists.length; i++) {
+            songFullName += artists[i] + " ";
+        }
+        return songFullName + trackName;
+    }
+
+    public String[] getPlaylistItems(String playlistId) throws ParseException, SpotifyWebApiException, IOException {
+        GetPlaylistRequest request = spotifyApi.getPlaylist(playlistId).build();
+        final Paging<PlaylistTrack> tracks = request.execute().getTracks();
+        final PlaylistTrack[] items = tracks.getItems();
+        String[] result = new String[tracks.getItems().length];
+        for (int i = 0; i < items.length; i++) {
+            result[i] = getTitleAndAuthorFromSpotifyApi(items[i].getTrack().getId());
+        }
+
+        return result;
     }
 }
