3160ff543261b142777b400d70498deceb9c62f8
Odeyalo <theprogrammerinfuture@gmail.com>
2021-09-03 19:37:45 +0300

    Code refactoring

--- src/main/java/command/commands/PlayCommand.java
+++ src/main/java/command/commands/PlayCommand.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.cglib.transform.AbstractProcessTask;
 import sendmessages.SendMessageInterface;
+import services.PlayCommandService;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -24,15 +25,11 @@ public class PlayCommand implements Command {
     private SendMessageInterface sendMessageInterface;
     private GuildMessageReceivedEvent event;
     private ConnectToTheVoiceChannelService connect;
-    private PlayerManager playerManager;
-    private SearchVideoOnYoutubeImpl youtubeMusicSearcher;
-    private PlayMusicFromSpotify spotify;
+    private PlayCommandService playCommandService;
 
     public PlayCommand(SendMessageInterface sendMessageInterface) {
         this.sendMessageInterface = sendMessageInterface;
-        this.playerManager = new PlayerManager();
-        this.youtubeMusicSearcher = new SearchVideoOnYoutubeImpl();
-        this.spotify = new PlayMusicFromSpotify();
+        playCommandService = new PlayCommandService();
     }
 
     @Override
@@ -43,7 +40,7 @@ public class PlayCommand implements Command {
         connect.connectToTheVoiceChannel(event);
         PlayerManager.getInstance().setEvent(event);
         try {
-            distribution(url);
+            playCommandService.distribution(url, event);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -57,50 +54,5 @@ public class PlayCommand implements Command {
             return s.substring(5).trim();
         sendMessageInterface.sendMessage(event, "enter sound name");
         throw new SoundNotFoundException();
-    }
-
-    public void play(String trackUrl) {
-        System.out.println("play:");
-        PlayerManager.getInstance().loadAndPlay(event.getChannel(), trackUrl);
-    }
-
-    private void distribution(String text) throws IOException {
-        try {
-            new URL(text);
-            if (isSpotifyURL(text)) {
-                if (isSpotifyPlaylist(text)) {
-                    String playlistId = text.substring(34, text.lastIndexOf('?'));
-                    System.out.println(playlistId);
-                    final String[] playlistItems = spotify.getPlaylistItems(playlistId);
-                    for (int i = 0; i < playlistItems.length; i++) {
-                        final String item = playlistItems[i];
-                        System.out.println(item);
-                        final String video = youtubeMusicSearcher.searchVideoByKeyword(item);
-                        play(video);
-                    }
-                } else {
-                    String songId = text.substring(25, text.lastIndexOf('?'));
-                    final String titleAndAuthorFromSpotifyApi = spotify.getTitleAndAuthorFromSpotifyApi(songId);
-                    final String songURL = youtubeMusicSearcher.searchVideoByKeyword(titleAndAuthorFromSpotifyApi);
-                    play(songURL);
-                }
-            } else {
-                play(text);
-            }
-        } catch (MalformedURLException e) {
-            play(youtubeMusicSearcher.searchVideoByKeyword(text));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (SpotifyWebApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public boolean isSpotifyURL(String url) {
-        return url.startsWith("https://open.spotify.com/");
-    }
-
-    public boolean isSpotifyPlaylist(String url) {
-        return url.startsWith("https://open.spotify.com/playlist/");
     }
 }
